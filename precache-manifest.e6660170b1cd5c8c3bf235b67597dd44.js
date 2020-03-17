a8f0d8e1f73b94ac62d5ea18c5e1d28fd40a2fb8
Leonardo Alves <leonardo@gero.com.br>
2020-03-17 12:10:01 -0300

    Updates

---
+++ precache-manifest.e6660170b1cd5c8c3bf235b67597dd44.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33443002b6f4deba21d9c3322ecb3a90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3668ced11c11d6e72e9a",
+    "url": "/static/css/main.1610698b.chunk.css"
+  },
+  {
+    "revision": "7a9dac6cfcf63025e9a9",
+    "url": "/static/js/2.087c9ad7.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.087c9ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3668ced11c11d6e72e9a",
+    "url": "/static/js/main.39df25ec.chunk.js"
+  },
+  {
+    "revision": "6286d7163df321efe175e9f4f047310a",
+    "url": "/static/js/main.39df25ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f4701781ceece45964",
+    "url": "/static/js/runtime-main.61168126.js"
+  },
+  {
+    "revision": "4d3d945d4ee68dacfe558a78d6fdae11",
+    "url": "/static/media/background-login.4d3d945d.jpg"
+  },
+  {
+    "revision": "8546c73b40374c65923e6cd1d361e80e",
+    "url": "/static/media/logo.8546c73b.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
